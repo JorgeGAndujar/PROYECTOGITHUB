d7c411b08edd1fa7806e2278ec5801ecddbcfe8b
JorgeGAndujar <iyejorge@gmail.com>
2024-07-02 09:58:50 +0200

    FOTO 2

--- src/misrc/Principal.java
+++ src/misrc/Principal.java
@@ -5,5 +5,7 @@ public class Principal {
 
     public static void main(String[] args) {
         System.out.println("HOLA MUNDO");
+        
+        System.out.println("HOLA JORGEGANDUJAR");
     } 
 }
